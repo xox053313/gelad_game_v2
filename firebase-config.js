d69c7339aacb317b70189e420f03c75d27d3dfe1
xox053313 <77908805+xox053313@users.noreply.github.com>
2026-02-17 14:37:18 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,1 @@
+console.log("Firebase جاهز لاحقاً");
---
+++ style.css
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+function startGame() {
+    canvas.style.display = "block";
+    ctx.fillStyle = "red";
+    ctx.fillRect(180, 180, 40, 40);
+}
